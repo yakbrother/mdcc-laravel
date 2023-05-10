097f2891ea48edde68436a2f6dfdb9e2adf7a630
Tim Eaton <tim@reports-inc.com>
2023-05-10 09:48:14 +0200

    About page

--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -6,9 +6,83 @@ import MdccLayout from "../Layouts/MdccLayout.vue";
     <MdccLayout>
         <article>
             <h2>About</h2>
-            <p>
-                List of people....
+            <p class="text-xl">
+                Meet the people who made our chess club possible.
             </p>
+
+            <div class="profile flex items-center even:flex-row-reverse even:textright">
+                <img src="https://robohash.org/Maxine Waterspoon" />
+                <div>
+                    <h3>
+                        Maxine Waterspoon
+                    </h3>
+                    <div class="role">Founder and President</div>
+                    <p class="desc">
+                        A world-renowned expert on bubble baths, and has written several books on the subject. When she's
+                        not soaking in a tub of lavender-scented bubbles, she's usually playing the ukulele or practicing
+                        her juggling skills.
+                    </p>
+                </div>
+            </div>
+
+            <div class="profile flex items-center even:flex-row-reverse even:textright">
+                <img src="https://robohash.org/Vincent Thunderbolt" />
+                <div>
+                    <h3>
+                        Vincent Thunderbolt
+                    </h3>
+                    <div class="role">Resident Grandmaster</div>
+                    <p class="desc">
+                        A self-proclaimed pastry enthusiast who spends most of his days testing out new cupcake recipes in
+                        his kitchen. He's also a bit of a daredevil, and has been known to eat frosting straight out of the
+                        container, much to the chagrin of his dentist.
+                    </p>
+                </div>
+            </div>
+
+            <div class="profile flex items-center even:flex-row-reverse even:textright">
+                <img src="https://robohash.org/Penelope Nightingale" />
+                <div>
+                    <h3>
+                        Penelope Nightingale
+                    </h3>
+                    <div class="role">Treasurer</div>
+                    <p class="desc">
+                        A retired wrestler who now spends her days working as a cheese sculptor. She's won numerous awards
+                        for her cheese art, and her favorite sculpture to date is a life-size replica of Dwayne "The Rock"
+                        Johnson made entirely out of cheddar.
+                    </p>
+                </div>
+            </div>
+
+            <div class="profile flex items-center even:flex-row-reverse even:textright">
+                <img src="https://robohash.org/Jasper McAllister" />
+                <div>
+                    <h3>
+                        Jasper McAllister
+                    </h3>
+                    <div class="role">Event Organizer</div>
+                    <p class="desc">
+                        Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh,
+                        viverra non, semper suscipit, posuere a, pede.
+                    </p>
+                </div>
+            </div>
+
+            <div class="profile flex items-center even:flex-row-reverse even:textright">
+                <img src="https://robohash.org/Luna Starshine" />
+                <div>
+                    <h3>
+                        Luna Starshine
+                    </h3>
+                    <div class="role">Mascot</div>
+                    <p class="desc">
+                        A professional comedian who's been known to make audiences laugh until they cry (or until they pee
+                        their pants, whichever comes first). She's also an amateur tightrope walker, but she's not very good
+                        at it, so she mainly sticks to making people laugh.
+                    </p>
+                </div>
+            </div>
         </article>
 
     </MdccLayout>
