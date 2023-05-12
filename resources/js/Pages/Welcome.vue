299e8f3e9e48b4ba0eb50066fecf84ec1ed680e6
Tim Eaton <tim@reports-inc.com>
2023-05-12 21:30:52 +0200

    Start hero code

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -4,14 +4,42 @@ import MdccLayout from "../Layouts/MdccLayout.vue";
 
 <template>
     <MdccLayout>
-        <section id="hero">
-            <h1>Play, learn, and grow with our chess club.</h1>
-            <button href="/join">
-            Join Today >
-            </button>
+        <section
+            class="relative bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat">
+            <div
+                class="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l">
+            </div>
+
+            <div class="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
+                <div class="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+                    <h1 class="text-3xl font-extrabold sm:text-5xl">
+                        Play, learn, and grow
+
+                        <strong class="block font-extrabold text-rose-700">
+                            with our chess club.
+                        </strong>
+                    </h1>
+
+                    <p class="mt-4 max-w-lg sm:text-xl/relaxed">
+                        Whether you are a seasoned player or just starting out, our club provides a welcoming and friendly
+                        environment where you can hone your skills, meet fellow chess lovers, and participate in exciting
+                        tournaments and events!
+                    </p>
+
+                    <div class="mt-8 flex flex-wrap gap-4 text-center">
+                        <a href="#"
+                            class="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto">
+                            Join
+                        </a>
+
+                        <a href="#"
+                            class="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-rose-600 shadow hover:text-rose-700 focus:outline-none focus:ring active:text-rose-500 sm:w-auto">
+                            Read our Blog
+                        </a>
+                    </div>
+                </div>
+            </div>
         </section>
-        <!-- <img class="flex-1 w-full object-cover" !!">
-                         <div class="flex-[2] details"> !!" !#div> -->
         <section>
             <article>
                 <p>
